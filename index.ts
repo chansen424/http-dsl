2bc2c280f547fad40cd070394b896e3c5bc9b6d1
Christopher Hansen <chansen424@yahoo.com>
2020-11-07 16:37:25 -0500

    Add JSON GET Requests
    Breaking changes for printing

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { CharStreams, CommonTokenStream } from "antlr4ts";
-import { TerminalNode } from "antlr4ts/tree/TerminalNode";
+import fetch from 'node-fetch';
 import * as lexer from "./generated/httpLexer";
 import * as parser from "./generated/httpParser";
 
 let code = 'print({ "age" : 5, "type" : "a" })';
+// let code = '//hello\r\nGET from "https://aws.random.cat/meow?ref=apilist.fun"';
+
 let inputStream = CharStreams.fromString(code);
 let l = new lexer.httpLexer(inputStream);
 let tokenStream = new CommonTokenStream(l);
@@ -18,14 +20,29 @@ function isNumeric(s: string): boolean {
   return !isNaN(parseFloat(s));
 }
 
-function evaluateExpression(e: parser.ExpressionContext): ExpressionValue {
+function removeQuotes(s: string): string {
+  return s.substr(1, s.length - 2);
+}
+
+async function getRequest(s: string): Promise<Object> {
+  const url = new URL(removeQuotes(s));
+  return fetch(url).then(res => res.json());
+}
+
+/* Need to change this somehow so that everything that gets printed
+   doesn't wrap itself in Promise { }.
+*/
+async function evaluateExpression(e: parser.ExpressionContext): Promise<ExpressionValue> {
   if (e.request()) {
+    if (e.request()!.GET()) {
+      return await getRequest(e.request()!.STRING().text);
+    }
     return null;
   } else if (e.value()) {
     return evaluateValue(e.value()!);
   } else {
     let stringExpression = e.text;
-    return stringExpression.substr(1, stringExpression.length - 2);
+    return removeQuotes(stringExpression);
   }
 }
 
