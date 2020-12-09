edcd9b360472b440edd5940069f8901adbde45a4
Christopher Hansen <chansen424@yahoo.com>
2020-12-08 21:29:40 -0500

    Remove old parseJSON

--- src/utils.ts
+++ src/utils.ts
@@ -43,15 +43,6 @@ function parseArray(stringifiedArray: string, context: any): Object {
   return Array.from(result.values());
 }
 
-// function parseJSON(s: string, context: any): Object {
-//   return JSON.parse(s, function(_, value) {
-//     if (typeof(value) === 'string' && value[0] === '$'){
-//       const variableName = value.substr(1)
-//       return context[variableName]
-//     }
-//     return value
-//   });
-// }
 function parseJson(stringifiedJson: string, context: any): Object {
   const variablesInserted = stringifiedJson.replace(
     variableInJSON,
