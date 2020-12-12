12ecbdcd970ab0c9ad554345e6be272b444ec114
Christopher Hansen <chansen424@yahoo.com>
2020-12-12 15:09:18 -0500

    Add INVALID_HEADER error

--- src/errors.ts
+++ src/errors.ts
@@ -2,4 +2,5 @@ const VAR_NOT_FOUND = (x: string) => new Error(`Variable not found: ${x}`);
 const ILLEGAL_EXTRACTION = new Error("Can only extract attributes from a JSON");
 const ILLEGAL_SET = new Error("Can only add or edit fields on JSONs");
 const UNDEFINED_PARSER_CONTEXT = new Error("Cannot operate on undefined parser context");
-export { VAR_NOT_FOUND, ILLEGAL_EXTRACTION, ILLEGAL_SET, UNDEFINED_PARSER_CONTEXT };
+const INVALID_HEADER = new Error("Header values can only be strings!");
+export { VAR_NOT_FOUND, ILLEGAL_EXTRACTION, ILLEGAL_SET, UNDEFINED_PARSER_CONTEXT, INVALID_HEADER };
